7bea1d125f0738c0b736613b9cbda02e495dc9c5
leolamien <sienou.lamien@student.vinci.be>
2022-12-14 14:58:33 +0100

    login

--- Frontend/src/app/Auths/login/login.component.ts
+++ Frontend/src/app/Auths/login/login.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../auth.service";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  form: any = {
+    email: null,
+    password: null,
+  };
 
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    alert("test")
+    const {email, password } =
+      this.form;
+    //console.log(firstname, lastname, email, password, birthDate );
+    // 0 is the rankingPoints because it is a new user
+    this.authService
+    .login(email, password)
+    .subscribe({
+      next: (data) => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      error: (err) => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      },
+    });
+  }
 }
